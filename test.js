c56817121795e2c4311147dc8e0acbeaa6326b42
Marc Harter <wavded@gmail.com>
2015-12-11 13:05:02 -0600

    Switch to babel-register/babel-polyfill

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ import test from 'tape'
 
 test('babel-tape-runner', t => {
   let file = readFileSync(__filename, 'utf8')
+  t.ok(/hello/gim.flags, 'gim')
   t.ok(/^import/.test(file))
   t.end()
 })
