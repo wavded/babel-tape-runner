62d42a528ac5f70247f8388d90ff1535f9e1aac0
Stephen Kawaguchi <stephen.kawaguchi@gmail.com>
2016-06-10 16:04:31 -0400

    feat: Add --files and --no-css flags
    - Introduces a dependency on the `ignore-styles` package
    - This allows us to easily support ignoring css when babel is also used to compile css
    - Ideally we would expose things more elegantly, but this is a quick fix for anyone out there that was stuck like me
    
    feat: Added --file as another flag in the args
    
    chore: Updated the readme to point out --no-css
    
    chore: Named unnamed test
    
    refactor: Extracted the flags
    
    fix: Fixed the unit tests to match the new API

--- test.js
+++ test.js
@@ -4,6 +4,6 @@ import test from 'tape'
 test('babel-tape-runner', t => {
   let file = readFileSync(__filename, 'utf8')
   t.ok(/hello/gim.flags, 'gim')
-  t.ok(/^import/.test(file))
+  t.ok(/^import/.test(file), 'import file')
   t.end()
 })
