0f34b3763c64c87f4e23d3cd0e8d07bdd5d6d6eb
Linus Unnebäck <linus@folkdatorn.se>
2018-10-10 20:44:49 +0200

    Prefer const over let

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs'
 import test from 'tape'
 
 test('babel-tape-runner', t => {
-  let file = readFileSync(__filename, 'utf8')
+  const file = readFileSync(__filename, 'utf8')
   t.ok(/hello/gim.flags, 'gim')
   t.ok(/^import/.test(file))
   t.end()
